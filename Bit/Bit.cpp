2417e4a79f7026df96d8643de6b4a41b632ad8bf
ParadoxMeltdown <jan.gabriel.ona@gmail.com>
2013-11-26 13:05:04 -0500

    File Name change

--- Bit/Bit.cpp
+++ Bit/Bit.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+using namespace std;
 
 //Header
-using namespace std;
 class bits {
   signed int _val;
 public:
@@ -11,13 +11,14 @@ public:
   bool operator[](int toval);
 };
 
-//Source
 bits::bits(int val) {
   _val = val;
 }
+
 void bits::setA(unsigned int& val, signed int pos, bool toval) {
     toval? (val = val | (1<<(pos-1))) : (val = val & ~(1<<(pos-1)));
 }
+
 bool bits::operator[](int bitNo) {
   return (_val & (1 << (bitNo))) != 0; 
 }
@@ -30,6 +31,7 @@ ostream& operator<<(ostream& os, bits src) {
   }
   return os;
 }
+
 int main(){
   bits B(0xf0f0f0f0);
   cout<<B[6]<<endl;
